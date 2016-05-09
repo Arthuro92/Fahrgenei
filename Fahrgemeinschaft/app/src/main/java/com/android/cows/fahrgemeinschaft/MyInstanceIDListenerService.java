7f2f400398e102c01b9df62a2caa7e513f9d2737
Lennart <y0059303@tu-braunschweig.de>
2016-05-09 14:37:47 +0200

    App with Google Example is working

--- Fahrgemeinschaft/app/src/main/java/com/android/cows/fahrgemeinschaft/MyInstanceIDListenerService.java
+++ Fahrgemeinschaft/app/src/main/java/com/android/cows/fahrgemeinschaft/MyInstanceIDListenerService.java
@@ -1,22 +1,40 @@
+/**
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.android.cows.fahrgemeinschaft;
 
 import android.content.Intent;
 
 import com.google.android.gms.iid.InstanceIDListenerService;
 
-/**
- * Created by Lennart on 02.05.2016.
- * handle the creation, rotation, and updating of registration tokens.
- */
 public class MyInstanceIDListenerService extends InstanceIDListenerService {
 
+    private static final String TAG = "MyInstanceIDLS";
+
     /**
-     * The listener service's onTokenRefresh method should be invoked if the GCM registration token has been refreshed:
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. This call is initiated by the
+     * InstanceID provider.
      */
+    // [START refresh_token]
     @Override
     public void onTokenRefresh() {
-        //fetch updated Instance ID token and notify our app´s server of any changes (if applicable)
+        // Fetch updated Instance ID token and notify our app's server of any changes (if applicable).
         Intent intent = new Intent(this, RegistrationIntentService.class);
         startService(intent);
     }
+    // [END refresh_token]
 }
--- Fahrgemeinschaft/app/src/main/java/com/android/cows/fahrgemeinschaft/RegistrationIntentService.java
+++ Fahrgemeinschaft/app/src/main/java/com/android/cows/fahrgemeinschaft/RegistrationIntentService.java
@@ -1,9 +1,26 @@
+/**
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.android.cows.fahrgemeinschaft;
 
 import android.app.IntentService;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 import com.google.android.gms.gcm.GcmPubSub;
@@ -12,31 +29,18 @@ import com.google.android.gms.iid.InstanceID;
 
 import java.io.IOException;
 
-/**
- * Created by Lennart on 03.05.2016.
- */
 public class RegistrationIntentService extends IntentService {
-
 
     private static final String TAG = "RegIntentService";
     private static final String[] TOPICS = {"global"};
 
-    /**
-     * Default Konstruktor
-     */
     public RegistrationIntentService() {
         super(TAG);
     }
 
-    /**
-     * To obtain a token, call instanceID.getToken, providing the app server's sender ID and setting the scope
-     * to GoogleCloudMessaging.INSTANCE_ID_SCOPE. Do not call this method in the main thread;
-     * instead, use a service that extends IntentService as shown:
-     * @param intent 
-     */
-     @Override
+    @Override
     protected void onHandleIntent(Intent intent) {
-         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 
         try {
             // [START register_for_gcm]
@@ -51,7 +55,7 @@ public class RegistrationIntentService extends IntentService {
             // [END get_token]
             Log.i(TAG, "GCM Registration Token: " + token);
 
-
+            // TODO: Implement this method to send any registration to your app's servers.
             sendRegistrationToServer(token);
 
             // Subscribe to topic channels
@@ -62,26 +66,42 @@ public class RegistrationIntentService extends IntentService {
             // otherwise your server should have already received the token.
             sharedPreferences.edit().putBoolean(QuickstartPreferences.SENT_TOKEN_TO_SERVER, true).apply();
             // [END register_for_gcm]
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            Log.d(TAG, "Failed to complete token refresh", e);
+            // If an exception happens while fetching the new token or updating our registration data
+            // on a third-party server, this ensures that we'll attempt the update at a later time.
+            sharedPreferences.edit().putBoolean(QuickstartPreferences.SENT_TOKEN_TO_SERVER, false).apply();
         }
-
+        // Notify UI that registration has completed, so the progress indicator can be hidden.
+        Intent registrationComplete = new Intent(QuickstartPreferences.REGISTRATION_COMPLETE);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(registrationComplete);
     }
 
     /**
-     * subscribe to new Topic
-     * @param token Topic subscribing to
-     * @throws IOException
+     * Persist registration to third-party servers.
+     *
+     * Modify this method to associate the user's GCM registration token with any server-side account
+     * maintained by your application.
+     *
+     * @param token The new token.
      */
+    private void sendRegistrationToServer(String token) {
+        // Add custom implementation, as needed.
+    }
+
+    /**
+     * Subscribe to any GCM topics of interest, as defined by the TOPICS constant.
+     *
+     * @param token GCM token
+     * @throws IOException if unable to reach the GCM PubSub service
+     */
+    // [START subscribe_topics]
     private void subscribeTopics(String token) throws IOException {
         GcmPubSub pubSub = GcmPubSub.getInstance(this);
         for (String topic : TOPICS) {
             pubSub.subscribe(token, "/topics/" + topic, null);
         }
     }
-
-    private void sendRegistrationToServer(String token) {
-        // TODO: Implement this method to send any registration to your app's servers.
-    }
+    // [END subscribe_topics]
 
 }
