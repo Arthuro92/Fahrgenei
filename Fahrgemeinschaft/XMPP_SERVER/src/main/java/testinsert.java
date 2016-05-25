c6332273a69a38c382df8f2ba27eb29c156b65c6
Lennart <y0059303@tu-braunschweig.de>
2016-05-25 19:54:58 +0200

    merged ChatObserver with GroupOverview and AppointmentOverview

---
+++ Fahrgemeinschaft/XMPP_SERVER/src/main/java/testinsert.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Created by Lennart on 24.05.2016.
+ */
+public class testinsert {
+
+    public testinsert(String id, String object) {
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection( "jdbc:mysql://localhost:3306/testschema", "testuser", "android" );
+            Statement stmt = con.createStatement();
+            String query = "INSERT INTO gruppe (gid, objectstring) VALUES ("+ id + "," + object + ")";
+            stmt.executeUpdate( query );
+        }
+
+        catch ( SQLException e )
+        {
+            e.printStackTrace();
+        }
+    }
+}
