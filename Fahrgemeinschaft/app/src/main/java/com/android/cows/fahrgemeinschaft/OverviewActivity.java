ef42999958f53abe18e496e7fe4d146365f4746a
Lennart <y0059303@tu-braunschweig.de>
2016-05-27 00:16:33 +0200

    Merged with Irina_neu Branch and Launcher Activity is OverviewActivity, next step is to get token for gcm before going to overview

--- Fahrgemeinschaft/app/src/main/java/com/android/cows/fahrgemeinschaft/OverviewActivity.java
+++ Fahrgemeinschaft/app/src/main/java/com/android/cows/fahrgemeinschaft/OverviewActivity.java
@@ -1,15 +1,11 @@
 package com.android.cows.fahrgemeinschaft;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.GridView;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
 public class OverviewActivity extends AppCompatActivity {
     private static final String TAG = "OverviewActivity";
@@ -20,74 +16,86 @@ public class OverviewActivity extends AppCompatActivity {
 
 
         ImageButton groupButton = (ImageButton) findViewById(R.id.groupButton);
-        groupButton.setOnClickListener(new View.OnClickListener() {
+        if (groupButton != null) {
+            groupButton.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View view) {
-                Intent nextToGroup = new Intent(OverviewActivity.this, GroupActivity.class);
-                Log.i(TAG,"OnClickGroup");
-                startActivity(nextToGroup);
+                @Override
+                public void onClick(View view) {
+                    Intent nextToGroup = new Intent(OverviewActivity.this, GroupOverview.class);
+                    Log.i(TAG,"OnClickGroup");
+                    startActivity(nextToGroup);
 
-            }
-        });
+                }
+            });
+        }
 
         ImageButton calendarButton = (ImageButton) findViewById(R.id.calendarButton);
-        calendarButton.setOnClickListener(new View.OnClickListener() {
+        if (calendarButton != null) {
+            calendarButton.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View view) {
-                Intent nextToCalendar = new Intent(OverviewActivity.this, CalendarActivity.class);
-                startActivity(nextToCalendar);
+                @Override
+                public void onClick(View view) {
+                    Intent nextToCalendar = new Intent(OverviewActivity.this, AppointmentOverview.class);
+                    startActivity(nextToCalendar);
 
-            }
-        });
+                }
+            });
+        }
 
         ImageButton messageButton = (ImageButton) findViewById(R.id.messageButton);
-        messageButton.setOnClickListener(new View.OnClickListener() {
+        if (messageButton != null) {
+            messageButton.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-                Intent nextToMessage = new Intent(OverviewActivity.this, MessageActivity.class);
-                startActivity(nextToMessage);
+                @Override
+                public void onClick(View v) {
+//                    Intent nextToMessage = new Intent(OverviewActivity.this, AppointmentOverview.class);
+//                    startActivity(nextToMessage);
 
-            }
-        });
+                }
+            });
+        }
 
 
         ImageButton notesButton = (ImageButton) findViewById(R.id.notesButton);
-        notesButton.setOnClickListener(new View.OnClickListener() {
+        if (notesButton != null) {
+            notesButton.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-                Intent nextToNotes = new Intent(OverviewActivity.this, NotesActivity.class);
-                startActivity(nextToNotes);
+                @Override
+                public void onClick(View v) {
+//                    Intent nextToNotes = new Intent(OverviewActivity.this, NotesActivity.class);
+//                    startActivity(nextToNotes);
 
-            }
-        });
+                }
+            });
+        }
 
         ImageButton settingsButton = (ImageButton) findViewById(R.id.settingsButton);
-        settingsButton.setOnClickListener(new View.OnClickListener() {
+        if (settingsButton != null) {
+            settingsButton.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-                Intent nextToSettings = new Intent(OverviewActivity.this, SettingsActivity.class);
-                startActivity(nextToSettings);
+                @Override
+                public void onClick(View v) {
+                    Intent nextToSettings = new Intent(OverviewActivity.this, SettingsActivity.class);
+                    startActivity(nextToSettings);
 
-            }
-        });
+                }
+            });
+        }
 
 
         ImageButton mailButton = (ImageButton) findViewById(R.id.mailButton);
-        mailButton.setOnClickListener(new View.OnClickListener() {
+        if (mailButton != null) {
+            mailButton.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View view) {
-                Intent nextToCalendar = new Intent(OverviewActivity.this, CalendarActivity.class);
-                startActivity(nextToCalendar);
+                @Override
+                public void onClick(View view) {
+//                    Intent nextToCalendar = new Intent(OverviewActivity.this, CalendarActivity.class);
+//                    startActivity(nextToCalendar);
 
-            }
+                }
 
-            });
+                });
+        }
 
     }
 }
