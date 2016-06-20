6e35d1b318325ccb14a828352d2f403e6d8e714e
cemcosgun <y0059811@tu-bs.de>
2016-06-20 23:04:11 +0200

    Layout anpassungen und Actionbar angelegt (mit funktionen hinterlegt)

--- Fahrgemeinschaft/app/src/main/java/com/android/cows/fahrgemeinschaft/GeneralTabsActivity.java
+++ Fahrgemeinschaft/app/src/main/java/com/android/cows/fahrgemeinschaft/GeneralTabsActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class GeneralTabsActivity extends AppCompatActivity {
@@ -13,10 +16,15 @@ public class GeneralTabsActivity extends AppCompatActivity {
     ViewPager viewPagerGeneral;
     TabLayout tabLayoutGeneral;
 
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_general_group_tabs);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
 
 
         viewPagerGeneral = (ViewPager) findViewById(R.id.viewpagerGeneral);
@@ -50,8 +58,30 @@ public class GeneralTabsActivity extends AppCompatActivity {
         });
     }
 
-    public void createGroup(View view) {
-        Intent intent = new Intent(this, CreateGroupActivity.class);
-        startActivity(intent);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        if (id == R.id.action_addUser) {
+            Intent intent = new Intent(this, CreateGroupActivity.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
