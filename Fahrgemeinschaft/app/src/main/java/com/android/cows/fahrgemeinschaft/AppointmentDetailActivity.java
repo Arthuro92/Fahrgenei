29636e66a01501d8379c7454e7d12e7a5eca9b42
Arthur <a.illi@tu-bs.de>
2016-06-23 00:19:22 +0200

    Design  der TaskDetailAcitivty und AppointmentDetailAcitivity verbessert

--- Fahrgemeinschaft/app/src/main/java/com/android/cows/fahrgemeinschaft/AppointmentDetailActivity.java
+++ Fahrgemeinschaft/app/src/main/java/com/android/cows/fahrgemeinschaft/AppointmentDetailActivity.java
@@ -2,6 +2,8 @@ package com.android.cows.fahrgemeinschaft;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -17,13 +19,18 @@ public class AppointmentDetailActivity extends AppCompatActivity {
     public TextView terminTreffOrt;
     public TextView terminAbfahrtZeit;
     public TextView terminZielOrt;
+    public Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appointment_detail);
 
-        TextView terminName = (TextView) findViewById(R.id.terminName);
+        toolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
+        setSupportActionBar(toolbar);
+
+
+        //TextView terminName = (TextView) findViewById(R.id.terminName);
         TextView terminTreffZeit = (TextView) findViewById(R.id.terminTreffZeit);
         TextView terminTreffOrt = (TextView) findViewById(R.id.terminTreffOrt);
         TextView terminAbfahrtZeit = (TextView) findViewById(R.id.terminAbfahrtZeit);
@@ -37,11 +44,19 @@ public class AppointmentDetailActivity extends AppCompatActivity {
         String startingTimeFormated = format1.format( startingtimeCal.getTime()) +" Uhr";
         String meetingtimeFormated = format1.format( meetingtimeCal.getTime()) +" Uhr";
 
-        terminName.setText(bundle.getString("name"));
+//        terminName.setText(bundle.getString("name"));
         terminAbfahrtZeit.setText(startingTimeFormated);
         terminTreffOrt.setText(bundle.getString("meetingpoint"));
         terminTreffZeit.setText(meetingtimeFormated);
         terminZielOrt.setText(bundle.getString("destination"));
+        setTitle(bundle.getString("name"));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_appointment_detail, menu);
+        return true;
     }
 
 }
