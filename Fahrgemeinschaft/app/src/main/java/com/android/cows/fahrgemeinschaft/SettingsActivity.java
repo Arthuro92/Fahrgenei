bf939c25cdc7646c9048cdda8372b8c7461b6e67
irinahuber <i.huber@tu-bs.de>
2016-06-23 15:33:50 +0200

    SettingsActivity hat soweit alle Inhalte. Es liegen noch keine Funktionen hinter.

--- Fahrgemeinschaft/app/src/main/java/com/android/cows/fahrgemeinschaft/SettingsActivity.java
+++ Fahrgemeinschaft/app/src/main/java/com/android/cows/fahrgemeinschaft/SettingsActivity.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.android.gms.auth.api.Auth;
@@ -15,7 +18,7 @@ import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     public static final String TAG = "SettingsActivity";
 
@@ -26,10 +29,22 @@ public class SettingsActivity extends AppCompatActivity {
 
     private String text;
 
+    private Spinner spinner;
+    private static final String[]paths = {"1", "4", "6", "8"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        spinner = (Spinner)findViewById(R.id.sitzAnzahl_spinner);
+        ArrayAdapter<String>adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item,paths);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
 
         SharedPreferences prefs = this.getSharedPreferences("com.android.cows.fahrgemeinschaft", Context.MODE_PRIVATE);
 
@@ -37,13 +52,34 @@ public class SettingsActivity extends AppCompatActivity {
         Log.i(TAG, prefs.getString("useremail", ""));
         Log.i(TAG, prefs.getString("userid", ""));
 
-        TextView username = (TextView) findViewById(R.id.userNameinhalt);
-        TextView email = (TextView) findViewById(R.id.emailinhalt);
+        TextView username = (TextView) findViewById(R.id.userNameid);
+        TextView email = (TextView) findViewById(R.id.emailid);
         username.setText(" " + prefs.getString("username", ""));
         email.setText(" " + prefs.getString("useremail", ""));
 
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+
+        switch (position) {
+            case 0:
+                // Whatever you want to happen when the first item gets selected
+                break;
+            case 1:
+                // Whatever you want to happen when the second item gets selected
+                break;
+            case 2:
+                // Whatever you want to happen when the thrid item gets selected
+                break;
+
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
+
 
 
     // [START signOut]
