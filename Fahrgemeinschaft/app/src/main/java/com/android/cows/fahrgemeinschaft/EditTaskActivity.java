c418f8aa8af2b460889cfe985f5e576e651f6373
Arthur <a.illi@tu-bs.de>
2016-07-06 23:56:00 +0200

    Edit Task funktioniert jetzt (GCMSEND Statements sind noch nicht ganz fertig)

--- Fahrgemeinschaft/app/src/main/java/com/android/cows/fahrgemeinschaft/EditTaskActivity.java
+++ Fahrgemeinschaft/app/src/main/java/com/android/cows/fahrgemeinschaft/EditTaskActivity.java
@@ -2,28 +2,35 @@ package com.android.cows.fahrgemeinschaft;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.android.cows.fahrgemeinschaft.gcm.MyGcmSend;
+
+import de.dataobjects.Appointment;
+import de.dataobjects.Task;
 
 public class EditTaskActivity extends AppCompatActivity {
     private static final String TAG = "EditTaskActivity";
 
-    public TextView terminName;
-    public TextView taskDescription;
-    public TextView taskAssignee;
+    public EditText terminName;
+    public EditText taskDescription;
+    public EditText taskAssignee;
     public Button saveTaskData;
     private Context context = GlobalAppContext.getAppContext();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_appointment);
+        setContentView(R.layout.activity_edit_task);
 
-        final TextView terminName = (TextView) findViewById(R.id.input_taskname);
+        final TextView taskName = (TextView) findViewById(R.id.input_taskname);
         final TextView taskDescription = (TextView) findViewById(R.id.input_description);
         final TextView taskAssignee = (TextView) findViewById(R.id.input_assignee);
         final Button savaTaskData = (Button) findViewById(R.id.saveTaskButton);
@@ -32,20 +39,32 @@ public class EditTaskActivity extends AppCompatActivity {
 
         Bundle bundle = getIntent().getExtras();
 
-        terminName.setText(bundle.getString("taskname"));
+        taskName.setText(bundle.getString("taskname"));
         taskDescription.setText(bundle.getString("taskdescription"));
         taskAssignee.setText(bundle.getString("taskincharge"));
+        final int tid = (int) bundle.getSerializable("tid");
 
 
         savaTaskData.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 //@TODO SQLITE DB und SERVER DB MUESSEN DEN EINTRAG FÜR DEN TERMIN ÄNDERN
+                //newtask
+
+                /** Läuft  leider ab hier noch nicht
+                MyGcmSend gcmsend = new MyGcmSend();
+                SharedPreferences prefs = context.getSharedPreferences("com.android.cows.fahrgemeinschaft", Context.MODE_PRIVATE);
+                Task tsk = new Task(tid,tid, prefs.getString("currentgid",""), prefs.getString("currentgroupname", "") + " " + tid, taskName.getText().toString(), taskDescription.getText().toString(), taskAssignee.getText().toString());
+                */
+
                 Intent intent = new Intent(context, TaskDetailActivity.class);
                 Log.i("Taskdaten",taskDescription +" "+taskAssignee.getText());
-                intent.putExtra("taskname", terminName.getText().toString()  );
+                intent.putExtra("taskname", taskName.getText().toString()  );
                 intent.putExtra("taskdescription", taskDescription.getText().toString()  );
                 intent.putExtra("taskincharge", taskAssignee.getText().toString()  );
+               // gcmsend.send("task", "newtask", tsk, context);
+
                 startActivity(intent);
+                finish();
             }
         });
     }
